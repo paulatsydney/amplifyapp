72e79d9ce610d98159cb941150dfd8fb9605337d
paulatsydney <paulatsydney@gmail.com>
2022-01-10 20:07:23 +1100

    System Under Construction

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Deployment
+          System Under Construction
         </a>
       </header>
     </div>
